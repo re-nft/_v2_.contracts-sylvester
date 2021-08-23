d2ff16ab7bb343a1e0006d8a95ad425bd8683930
nazariyv <nazariy@inbox.ru>
2021-08-23 17:21:13 +0100

    registry interface

--- contracts/interfaces/IRegistry.sol
+++ contracts/interfaces/IRegistry.sol
@@ -1,16 +1,106 @@
 //SPDX-Identifier-License: MIT
 pragma solidity =0.8.7;
 
+import "./IResolver.sol";
+
+//              @@@@@@@@@@@@@@@@        ,@@@@@@@@@@@@@@@@
+//              @@@,,,,,,,,,,@@@        ,@@&,,,,,,,,,,@@@
+//         @@@@@@@@,,,,,,,,,,@@@@@@@@&  ,@@&,,,,,,,,,,@@@@@@@@
+//         @@@**********@@@@@@@@@@@@@&  ,@@@@@@@@**********@@@
+//         @@@**********@@@@@@@@@@@@@&  ,@@@@@@@@**********@@@@@@@@
+//         @@@**********@@@@@@@@@@@@@&       .@@@**********@@@@@@@@
+//    @@@@@@@@**********@@@@@@@@@@@@@&       .@@@**********@@@@@@@@
+//    @@@**********@@@@@@@@@@@@@&            .@@@@@@@@**********@@@
+//    @@@**********@@@@@@@@@@@@@&            .@@@@@@@@**********@@@@@@@@
+//    @@@@@@@@**********@@@@@@@@&            .@@@**********@@@@@@@@@@@@@
+//    @@@@@@@@//////////@@@@@@@@&            .@@@//////////@@@@@@@@@@@@@
+//         @@@//////////@@@@@@@@&            .@@@//////////@@@@@@@@@@@@@
+//         @@@//////////@@@@@@@@&       ,@@@@@@@@//////////@@@@@@@@@@@@@
+//         @@@%%%%%/////(((((@@@&       ,@@@(((((/////%%%%%@@@@@@@@
+//         @@@@@@@@//////////@@@@@@@@&  ,@@@//////////@@@@@@@@@@@@@
+//              @@@%%%%%%%%%%@@@@@@@@&  ,@@@%%%%%%%%%%@@@@@@@@@@@@@
+//              @@@@@@@@@@@@@@@@@@@@@&  ,@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@&        @@@@@@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@&        @@@@@@@@@@@@@@@@
+
 interface IRegistry {
     enum NFTStandard {
         E721,
         E1155
     }
 
-    // creates the lending structs
+    // 160, 168, 200, 216, 232, 240
+    struct Lending {
+        address payable lenderAddress;
+        uint8 maxRentDuration;
+        uint32 dailyRentPrice;
+        uint16 lentAmount;
+        uint16 availableAmount;
+        IResolver.PaymentToken paymentToken;
+    }
+
+    // 180, 212
+    struct Renting {
+        address payable renterAddress;
+        uint32 rentedAt;
+        uint16 rentAmount;
+    }
+
+    // creates the lending structs and adds them to the enumerable set
     function lend(
+        // this is purely for transfers
         NFTStandard[] nftStandard,
-        uint256[] tokenIDs,
-        address[] nftAddress
+        // the below is used for hashing
+        address[] nftAddress,
+        uint256[] tokenID
     ) external;
+
+    // creates the renting structs and adds them to the enumerable set
+    function rent(
+        address[] nftAddress,
+        uint256[] tokenID,
+        uint256[] lendingID
+    ) external payable;
+
+    function stopRent(
+        address[] nftAddress,
+        uint256[] tokenID,
+        uint256[] lendingID
+    ) external;
+
+    // get all the lending of an address
+    // loops through the lending enumerable set
+    function getLending(address lenderAddress) external view;
+
+    // get all the renting of an address
+    // loops through the renting enumerable set
+    function getRenting(address renterAddress) external view;
+
+    // returns the renter, given the nftAddress, tokenID and lendingID
+    // given the nftAddress, tokenID and lendingID, will return the renter(s)
+    function getRenter(
+        address nftAddress,
+        uint256 tokenID,
+        uint256 lendingID
+    ) external view;
 }
+
+//              @@@@@@@@@@@@@@@@        ,@@@@@@@@@@@@@@@@
+//              @@@,,,,,,,,,,@@@        ,@@&,,,,,,,,,,@@@
+//         @@@@@@@@,,,,,,,,,,@@@@@@@@&  ,@@&,,,,,,,,,,@@@@@@@@
+//         @@@**********@@@@@@@@@@@@@&  ,@@@@@@@@**********@@@
+//         @@@**********@@@@@@@@@@@@@&  ,@@@@@@@@**********@@@@@@@@
+//         @@@**********@@@@@@@@@@@@@&       .@@@**********@@@@@@@@
+//    @@@@@@@@**********@@@@@@@@@@@@@&       .@@@**********@@@@@@@@
+//    @@@**********@@@@@@@@@@@@@&            .@@@@@@@@**********@@@
+//    @@@**********@@@@@@@@@@@@@&            .@@@@@@@@**********@@@@@@@@
+//    @@@@@@@@**********@@@@@@@@&            .@@@**********@@@@@@@@@@@@@
+//    @@@@@@@@//////////@@@@@@@@&            .@@@//////////@@@@@@@@@@@@@
+//         @@@//////////@@@@@@@@&            .@@@//////////@@@@@@@@@@@@@
+//         @@@//////////@@@@@@@@&       ,@@@@@@@@//////////@@@@@@@@@@@@@
+//         @@@%%%%%/////(((((@@@&       ,@@@(((((/////%%%%%@@@@@@@@
+//         @@@@@@@@//////////@@@@@@@@&  ,@@@//////////@@@@@@@@@@@@@
+//              @@@%%%%%%%%%%@@@@@@@@&  ,@@@%%%%%%%%%%@@@@@@@@@@@@@
+//              @@@@@@@@@@@@@@@@@@@@@&  ,@@@@@@@@@@@@@@@@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@&        @@@@@@@@@@@@@@@@
+//                   @@@@@@@@@@@@@@@@&        @@@@@@@@@@@@@@@@
