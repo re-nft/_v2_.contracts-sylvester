34b879ba454037fabbfdbdd70d81ae7e3dd8a74d
nazariyv <nazariy@inbox.ru>
2022-02-03 18:05:35 +0000

    chore: add collateral branch
    Our original contracts repo uses hardhat. We have moved to python deploy
    / dev infra for Solidity contracts. I am creating this branch as a
    precursor to turning this repo into the contracts repo and archiving the
    other one. This repo will be the contracts repo for renft v1.

--- scripts/deploy_ropsten.py
+++ scripts/deploy_ropsten.py
@@ -4,62 +4,56 @@ from enum import Enum
 
 from brownie import (
     Resolver,
-    Registry,
-    E721,
-    E1155,
-    DAI,
-    USDC,
-    TUSD,
+    ReNFT,
     accounts,
-    chain,
 )
-
-
-class NFTStandard(Enum):
-    E721 = 0
-    E1155 = 1
 
 
 class PaymentToken(Enum):
     SENTINEL = 0
     DAI = 1
     USDC = 2
-    TUSD = 3
+
+
+    # constructor(
+    #     address _resolver,
+    #     address payable _beneficiary,
+    #     address _admin
+    # ) {
+    #     ensureIsNotZeroAddr(_resolver);
+    #     ensureIsNotZeroAddr(_beneficiary);
+    #     ensureIsNotZeroAddr(_admin);
+    #     resolver = IResolver(_resolver);
+    #     beneficiary = _beneficiary;
+    #     admin = _admin;
+    # }
+
+ROPSTEN_DAI = "0xad6d458402f60fd3bd25163575031acdce07538d" # 18 DP
+ROPSTEN_USDC = "0x07865c6e87b9f70255377e024ace6630c1eaa37f" # 6 DP
 
 
 def main():
 
-    a = accounts.load("algo_two")
-    beneficiary = a
-    admin = a
+    a = accounts.load("<account name>")
+    beneficiary, admin = a, a
 
     from_a = {"from": a}
 
     resolver = Resolver.deploy(a, from_a)
 
-    dai = DAI.deploy(from_a)
-    usdc = USDC.deploy(from_a)
-    tusd = TUSD.deploy(from_a)
+    resolver.setPaymentToken(PaymentToken.DAI.value, ROPSTEN_DAI)
+    resolver.setPaymentToken(PaymentToken.USDC.value, ROPSTEN_USDC)
+    resolver.setPaymentToken(PaymentToken.TUSD.value, ROPSTEN_TUSD)
 
-    resolver.setPaymentToken(PaymentToken.DAI.value, dai.address)
-    resolver.setPaymentToken(PaymentToken.USDC.value, usdc.address)
-    resolver.setPaymentToken(PaymentToken.TUSD.value, tusd.address)
-
-    registry = Registry.deploy(
-        resolver.address, beneficiary.address, admin.address, from_a
-    )
+    renft = ReNFT.deploy(resolver, beneficiary, admin, from_a)
 
     e721 = E721.deploy(from_a)
+
+    breakpoint()
+
     e721b = E721.deploy(from_a)
     e1155 = E1155.deploy(from_a)
     e1155b = E1155.deploy(from_a)
 
-    DAI.publish_source(dai)
-    USDC.publish_source(usdc)
-    TUSD.publish_source(tusd)
-    # E721.publish_source(e721)
-    # E721.publish_source(e721b)
-    # E1155.publish_source(e1155)
-    # E1155.publish_source(e1155b)
     Resolver.publish_source(resolver)
-    Registry.publish_source(registry)
+    ReNFT.publish_source(renft)
