448cd9a26ee74c6d76a16c572716c0ee42917ada
nazariyv <nazariy@inbox.ru>
2022-05-16 19:56:30 +0100

    chore: matic deploy

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -6,9 +6,36 @@ from brownie.network.gas.strategies import GasNowStrategy
 
 def main():
 
-    a = accounts.load("")
-    resolver = "0x945e589a4715d1915e6fe14f08e4887bc4019341"
-    beneficiary = "0x28f11c3D76169361D22D8aE53551827Ac03360B0"
-    gas_strategy = GasNowStrategy("fast")
-    from_a = {"from": a, "gas_price": gas_strategy}
-    _ = Registry.deploy(resolver, beneficiary, a.address, from_a)
+    a = accounts.load("renft-deployer")
+
+    from_a = {"from": a}
+
+    # ! if you have previously deployed a resolver
+    # ! just paste the address here
+    # resolver = "0x945e589a4715d1915e6fe14f08e4887bc4019341"
+    resolver = Resolver.deploy(a, from_a)
+
+    weth = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619" # 18 dp
+    dai  = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063" # 18 dp
+    usdc = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174" # 6 dp
+    usdt = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F" # 6 dp
+    tusd = "0x2e1AD108fF1D8C782fcBbB89AAd783aC49586756" # 18 dp
+
+    resolver.setPaymentToken(1, weth)
+    resolver.setPaymentToken(2, dai)
+    resolver.setPaymentToken(3, usdc)
+    resolver.setPaymentToken(4, usdt)
+    resolver.setPaymentToken(5, tusd)
+
+    # ! beenficiary on polygon is the deployer
+    beneficiary = "0x000000045232fe75A3C7db3e5B03B0Ab6166F425"
+
+    registry = Registry.deploy(
+        resolver.address,
+        beneficiary,
+        a.address,
+        from_a
+    )
+
+    # matic resolver: 0x6884d88ce56c5c93f46ee23684eba8628c90b518
+    # matic registry: 0xfa06cfe34c85ec6b6d29a6a99806cc68ba0018fe
