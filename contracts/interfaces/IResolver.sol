2f3307b8258fa6bdfa7c7ece2927fdb633decdb3
nazariyv <hi@naz.ooo>
2022-12-13 21:16:43 +0000

    chore: use uint8 instead of PaymentToken enum for extendability of payment tokens

--- contracts/interfaces/IResolver.sol
+++ contracts/interfaces/IResolver.sol
@@ -22,13 +22,6 @@ pragma solidity =0.8.7;
 //                   @@@@@@@@@@@@@@@@&        @@@@@@@@@@@@@@@@
 
 interface IResolver {
-    enum PaymentToken {
-        SENTINEL,
-        DAI,
-        USDC,
-        TUSD
-    }
-
     function getPaymentToken(uint8 _pt) external view returns (address);
 
     function setPaymentToken(uint8 _pt, address _v) external;
